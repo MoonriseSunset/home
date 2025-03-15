6ee4f2d7fa7c3088c33d65a97ea71cb5bff8ade1
Moonrise (Desktop) <aliuesch@gmail.com>
2025-03-14 21:37:16 -0700

    reconfigure site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'moonrisesunset.dev',
+  site: 'https://moonrisesunset.dev',
   base: '/home',
   integrations: [	
     starlight({
